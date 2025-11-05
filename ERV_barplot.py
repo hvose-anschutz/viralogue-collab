266447f88dabb0713afb2af303712d0c1b4de472
Holly Vose <holly.vose@cuanschutz.edu>
2025-11-04 17:22:31 -0700

    created ERV_barplot for graphing stacked barplots

---
+++ ERV_barplot.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+"""Makes a barplot of ERV family and species."""
+
+import re
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+my_erv_species = {}
+
+def find_erv(filename: str, mode: str) -> dict: 
+    if mode == "LINE":
+        my_regex = r"^L1"
+        df_dict = {"Name":["LINE"]}
+    elif mode == "ERV":
+        my_regex = r"ERV(1\D|L|K)"
+        df_dict = {"Name":["ERV"]}
+    else:
+        print("valid options are LINE or ERV.")
+        sys.exit(1)
+    with open(filename,"r",encoding="utf-8") as f:
+        for my_values in f:
+            line = my_values.strip().split("\t")
+            match_erv = re.search(my_regex,line[0])
+            if match_erv is not None:
+                if line[0] not in my_erv_species:
+                    my_erv_species[line[0]] = int(line[1])
+                else:
+                    my_erv_species[line[0]] += int(line[1])
+    f.close()
+
+    total_ervs = sum(my_erv_species.values())
+
+    for erv in my_erv_species:
+        df_dict[erv] = my_erv_species[erv] / total_ervs
+
+    return df_dict
+
+###############################################################
+# CHANGE THIS CODE
+
+df_dict = find_erv("SRR32782394_speciescount.txt",mode="TEST")
+outfile = "my_stacked_barplot.svg"
+save_figure = True
+###############################################################
+
+df = pd.DataFrame(df_dict)
+
+df_pivot = df.pivot_table(index="Name",values=df_dict.keys(),aggfunc=sum)
+
+my_plot = df_pivot.plot(kind="bar",stacked=True,grid=True)
+my_plot.set_axisbelow(True)
+plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+plt.title('ERV Species')
+plt.ylabel('Percentage of ERVs')
+plt.xticks(rotation=0)
+if save_figure:
+    plt.savefig(outfile, bbox_inches='tight')
+plt.show()
